ec352f39d6fa594c5ae36269dc63f7d4c4eb4a0e
Oliver <olichase91@gmail.com>
2023-03-06 17:14:30 -0800

    Small tweaks

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
-import { R_API_ID, R_API_KEY } from "./secrets.js";
+import { fetchRandomRecipes } from "./fetchRecipes.js";
+
+
+fetchRandomRecipes()
 
 
 
-let recipes = []
 
-
-const API_URL = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
-
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // do something with the data
-  })
-  .catch(error => console.error(`Fetch Error: ${error}`));
