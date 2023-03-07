1a75a3e44f4ad367f671cc5491fc014188a9b590
Oliver <olichase91@gmail.com>
2023-03-07 13:24:21 -0800

    Module structure removed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import { fetchRandomRecipes } from "./fetchRecipes.js";
 
+const BASE_URL = `https://api.edamam.com/api/recipes/v2`
+const RAND_RECI_URL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${R_API_ID}&app_key=${R_API_KEY}&imageSize=LARGE&random=true`
+
+let reciDataArr = []
 
 fetchRandomRecipes()
 
+
+
+     
+         function fetchRandomRecipes() {
+          fetch(RAND_RECI_URL)
+            .then(resp => resp.json())
+            .then(data => {
+              reciDataArr = data.hits
+              console.log(reciDataArr)
+            })
+            .catch(error => console.error(`Fetch Error: ${error}`))
+        }
+setTimeout(() => {
+    console.log(reciDataArr)
+  }, 2000)
 
 
 
--- src/render.js
+++
@@ -1,5 +0,0 @@
-
-
-export function renderRecipeCard(reciObj){
-    
-}
