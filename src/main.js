ffcc37960b8029d549ca0041776dfd070715e922
Oliver <olichase91@gmail.com>
2023-03-09 12:28:20 -0800

    Lots of styling, other little tweaks

--- src/main.js
+++ src/main.js
@@ -68,7 +68,6 @@ window.addEventListener('scroll', () => {
     throttledFetchReciSearch()
 }})
 //----------------------------------------------------
-
 // Fetch 20 random recipies and render them to the DOM ---------------------------------
 function fetchRandomRecipes() {
   fetch(RAND_RECI_URL)
@@ -83,14 +82,33 @@ function fetchRandomRecipes() {
   .catch(error => console.error(`Fetch Error: ${error}`))
 }
 //---------------------------------------------------
-
-//-----------------------------------------------------------
+// Capitailise first character in a string----------------------------------------
 function capFirstChar(str) {
   return str[0].toUpperCase() + str.slice(1)
   }
 //-------------------------------------------------
-
-//-----------------------------------------------------------------
+// Convert a string to title case--------------------------------------
+function toTitleCase(str){
+  let wordArr = str.split(' ')
+  let mappedWordArr = wordArr.map((word) => {
+    return word[0].toUpperCase() + word.slice(1)
+  })
+  return mappedWordArr.join(' ')
+}
+//----------------------------------------------------
+// Map a string and replace hyphens with spaces---------------------------------------
+function removeHyphens(str){
+  let mappedWordArr = str.split('-')
+  return mappedWordArr.join(' ')
+  }
+//-----------------------------------------------------
+// Map a string and replace commas with space, hyphen, space--------------------------
+function removeCommas(str){
+  let mappedWordArr = str.split(',')
+  return mappedWordArr.join(' - ')
+}
+//--------------------------------------------------------
+// Sorts Search Terms by Type...------------------------------------------------------
 function sortSearchTermsByType(arr1, arr2, arr3){
   for (let i = 0; i < arr1.length; i++) {
     if (arr2.includes(arr1[i])) {
@@ -99,7 +117,6 @@ function sortSearchTermsByType(arr1, arr2, arr3){
   }
 }
 //---------------------------------------------
-
 //-----------------------------------------------------
 function clearRecipes(){
   reciMenu.innerHTML = ''
@@ -185,7 +202,7 @@ if(specifiedSearchTerms.length === 0 && searchForm[0].value === ''){
 }
 //-------------------------------------------------------------------------------
 
-//--------------------------------------------------------------------------------------
+// Render Side Bar Text----------------------------------------------------------------
 function renderSearchFilterListToSideBar(typeArr, listTitle){
   const typeList = document.createElement('ul')
   const listHeading = document.createElement('h3')
@@ -195,7 +212,7 @@ function renderSearchFilterListToSideBar(typeArr, listTitle){
   for(let ele of typeArr){
     searchValues[ele] = false
     const listItem = document.createElement('li')
-    listItem.textContent = ele
+    listItem.textContent = toTitleCase(removeHyphens(ele))
     listItem.addEventListener('click', () => {
       if(searchValues[ele]){
         listItem.style.color = 'white'
@@ -217,7 +234,7 @@ function renderSearchFilterListToSideBar(typeArr, listTitle){
 
 // Render one recipe to the DOM ------------------------------------------------
 function renderReciCard(reciObj){
-  // reciCard events and PopUp-------
+  // reciCard events
   const reciCard = document.createElement('div')
   reciCard.addEventListener('mouseover',()=> {
     reciCard.style.border = '5px solid hotpink'
@@ -225,29 +242,46 @@ function renderReciCard(reciObj){
   reciCard.addEventListener('mouseout', ()=> {
     reciCard.style.border = '3px solid rgba(255, 255, 255, 0.856)'
   })
+  // PopUp ------------------------------
   reciCard.addEventListener('click', ()=> {
     const reciPopUp = document.createElement('div')
     reciPopUp.classList.add('reciPopUp')
-
-
+    //--------
     // Add Div for left side of PopUp
     const reciPopUpLeftDiv = document.createElement('div')
+    reciPopUpLeftDiv.classList.add('reciPopUpLeftDiv')
+    //--------
     // Add PopUp Image to left div
     const reciPopUpImg = document.createElement('img')
     reciPopUpImg.src = reciObj.recipe.images.LARGE.url
+    reciPopUpImg.classList.add('reciPopUpImg')
     reciPopUpLeftDiv.appendChild(reciPopUpImg)
+    //--------
     // Add Favorite Button to left div
     const reciPopUpFavBtn = document.createElement('button')
     reciPopUpFavBtn.textContent = ('Add To Favorites')
     reciPopUpFavBtn.addEventListener('click', () => {
       // Make this do stuff! 
     })
+    reciPopUpFavBtn.addEventListener('mouseenter', ()=> {
+      reciPopUpFavBtn.style.backgroundColor = ('hotpink')
+    })
+    reciPopUpFavBtn.addEventListener('mouseleave',()=> {
+      reciPopUpFavBtn.style.backgroundColor = ('rgba(0, 0, 0, 0.37)')
+    })
     reciPopUpLeftDiv.appendChild(reciPopUpFavBtn)
+    //--------
     // Add Recipe Link Button to left div
     const reciPopUpReciLinkBtn = document.createElement('button')
     reciPopUpReciLinkBtn.textContent = ('View Recipe')
     reciPopUpReciLinkBtn.addEventListener('click', () =>{
-      // Also Make this do stuff!!!
+      window.open(reciObj.recipe.url)
+    })
+    reciPopUpReciLinkBtn.addEventListener('mouseenter', ()=> {
+      reciPopUpReciLinkBtn.style.backgroundColor = ('hotpink')
+    })
+    reciPopUpReciLinkBtn.addEventListener('mouseleave',()=> {
+      reciPopUpReciLinkBtn.style.backgroundColor = ('rgba(0, 0, 0, 0.37)')
     })
     reciPopUpLeftDiv.appendChild(reciPopUpReciLinkBtn)
         // Add Nutritional Information Button to left div
@@ -256,21 +290,35 @@ function renderReciCard(reciObj){
     reciPopUpReciNutriBtn.addEventListener('click', () =>{
       // Also, also Make this do stuff!!!!!!!!
     })
+    reciPopUpReciNutriBtn.addEventListener('mouseenter', ()=> {
+      reciPopUpReciNutriBtn.style.backgroundColor = ('hotpink')
+    })
+    reciPopUpReciNutriBtn.addEventListener('mouseleave',()=> {
+      reciPopUpReciNutriBtn.style.backgroundColor = ('rgba(0, 0, 0, 0.37)')
+    })
     reciPopUpLeftDiv.appendChild(reciPopUpReciNutriBtn)
 
     // Add div for right side of popup
     const reciPopUpRightDiv = document.createElement('div')
+    reciPopUpRightDiv.classList.add('reciPopUpRightDiv')
     // Add div for top right
     const reciPopUpTopRightDiv = document.createElement('div')
+    reciPopUpTopRightDiv.classList.add('reciPopUpTopRightDiv')
     // Add Title to top right div
     const reciPopUpTitle = document.createElement('h2')
-    reciPopUpTitle.textContent = reciObj.recipe.label
+    reciPopUpTitle.textContent = toTitleCase(reciObj.recipe.label)
     reciPopUpTopRightDiv.appendChild(reciPopUpTitle)
     // Add Close button to top right div
     const reciPopUpCloseBtn = document.createElement('button')
     reciPopUpCloseBtn.textContent = ('X')
     reciPopUpCloseBtn.addEventListener('click', ()=> {
     reciPopUp.style.display = 'none'
+    })
+    reciPopUpCloseBtn.addEventListener('mouseenter', ()=>{
+      reciPopUpCloseBtn.style.backgroundColor = 'hotpink'
+    })
+    reciPopUpCloseBtn.addEventListener('mouseleave', ()=> {
+      reciPopUpCloseBtn.style.backgroundColor = 'rgba(0, 0, 0, 0.329)'
     })
     reciPopUpTopRightDiv.appendChild(reciPopUpCloseBtn)
     // Add ingredients list to right div
@@ -280,14 +328,19 @@ function renderReciCard(reciObj){
       listItem.textContent = ele
       reciPopUpIngList.appendChild(listItem)
     }
-    reciPopUpRightDiv.appendChild(reciPopUpIngList)
+    // Add caption to Ingredients List
+    const reciPopUpIngListCaption = document.createElement('caption')
+    reciPopUpIngListCaption.textContent = ('Ingredients List')
+    reciPopUpIngList.prepend(reciPopUpIngListCaption)
     // Stick It all Together
     reciPopUpRightDiv.appendChild(reciPopUpTopRightDiv)
+    reciPopUpRightDiv.appendChild(reciPopUpIngList)
     reciPopUp.appendChild(reciPopUpLeftDiv)
     reciPopUp.appendChild(reciPopUpRightDiv)
     reciMenu.appendChild(reciPopUp)
   })
-  //--------------
+  //----------------------
+  // Render Recipe Menu--------------------
 
   const reciCardInfo = document.createElement('div')
 
@@ -295,7 +348,7 @@ function renderReciCard(reciObj){
   reciImg.src = reciObj.recipe.images.REGULAR.url
 
   const reciTitle = document.createElement('h2')
-  reciTitle.textContent = reciObj.recipe.label
+  reciTitle.textContent = toTitleCase(reciObj.recipe.label)
 
   const reciCardList = document.createElement('ul')
   
@@ -303,7 +356,7 @@ function renderReciCard(reciObj){
   reciCardListCuType.textContent = `${capFirstChar(reciObj.recipe.cuisineType[0])} Cuisine`
 
   const reciCardListDietLab = document.createElement('li')
-  reciCardListDietLab.textContent = reciObj.recipe.dietLabels
+  reciCardListDietLab.textContent = reciObj.recipe.dietLabels[0]
 
   const reciCardListHealthLab = document.createElement('li')
   reciCardListHealthLab.textContent = reciObj.recipe.healthLabels[0]
@@ -315,7 +368,7 @@ function renderReciCard(reciObj){
   const reciCardFoot = document.createElement('footer')
 
   const reciCardFootDishType = document.createElement('span')
-  reciCardFootDishType.textContent = capFirstChar(reciObj.recipe.dishType[0])
+  reciCardFootDishType.textContent = toTitleCase(reciObj.recipe.dishType[0])
 
   const reciCardFootMealType = document.createElement('span')
   reciCardFootMealType.textContent = capFirstChar(reciObj.recipe.mealType[0])
@@ -324,27 +377,17 @@ function renderReciCard(reciObj){
   if(reciObj.recipe.totalTime == 0){
     reciCardFootPrepTime.textContent = 'Prep Time: 30 Minutes'
   }else{reciCardFootPrepTime.textContent = `Prep Time: ${reciObj.recipe.totalTime} Minutes`}
-
-  // const reciCardFootInfoBtn = document.createElement('button')
-  // reciCardFootInfoBtn.textContent = ("Click Here for More Info")
-  // reciCardFootInfoBtn.addEventListener('click',() =>{
-  //   window.open(reciObj.recipe.url)
-  // })
-
+  // Stick It all Together
   reciCardFoot.appendChild(reciCardFootDishType)
   reciCardFoot.appendChild(reciCardFootMealType)
   reciCardFoot.appendChild(reciCardFootPrepTime)
-  // reciCardFoot.appendChild(reciCardFootInfoBtn)
 
   reciCardInfo.appendChild(reciCardList)
   reciCardInfo.appendChild(reciCardFoot)
   reciCardInfo.appendChild(reciTitle)
 
-
   reciCard.appendChild(reciImg)
   reciCard.appendChild(reciCardInfo)
-  // reciCard.appendChild(reciCardList)
-  // reciCard.appendChild(reciCardFoot)
 
   reciCard.classList.add('reciCard')
   reciCardInfo.classList.add('reciCardInfo')
